24e48ae9317536d4a2687c9fc5e82140b27af603
lrz <1414556676@qq.com>
2020-08-03 18:04:24 +0800

    luckysheet

---
+++ public/luckysheet/demoData/sheetComment.js
@@ -0,0 +1,69 @@
+const sheetComment = {
+	"name": "Comment",
+	"color": "",
+	"config": {
+		"columlen": {
+			"2": 102
+		}
+	},
+	"index": "5",
+	"chart": [],
+	"status": "0",
+	"order": "5",
+	"column": 18,
+	"row": 36,
+	"celldata": [{
+		"r": 2,
+		"c": 2,
+		"v": {
+			"m": "HoverShown",
+			"ct": {
+				"fa": "General",
+				"t": "g"
+			},
+			"v": "HoverShown",
+			"bl": 1,
+			"ps": {
+				"left": null,
+				"top": null,
+				"width": null,
+				"height": null,
+				"value": "Hello world!",
+				"isshow": false
+			}
+		}
+	}, {
+		"r": 7,
+		"c": 2,
+		"v": {
+			"m": "Size",
+			"ct": {
+				"fa": "General",
+				"t": "g"
+			},
+			"v": "Size",
+			"bl": 1,
+			"ps": {
+				"left": null,
+				"top": null,
+				"width": null,
+				"height": null,
+				"value": "Hello,world!",
+				"isshow": true
+			}
+		}
+	}],
+	"visibledatarow": [],
+	"visibledatacolumn": [],
+	"ch_width": 4748,
+	"rh_height": 1790,
+	"luckysheet_select_save": [{
+		"row": [0, 0],
+		"column": [0, 0]
+	}],
+	"luckysheet_selection_range": [],
+	"scrollLeft": 0,
+	"scrollTop": 0
+}
+
+export default sheetComment;
--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
@@ -8,10 +7,23 @@
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 
+
 export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  data: ()=> {
+    return {
+      pen: null
+    }
+  },
+  created(){
+    
+  },
+  mounted() {
+  },
+  methods: {
   }
 }
 </script>
