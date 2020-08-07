ae8b28caa204f702b98f0a10e211b1b7fb1e866a
lrz <1414556676@qq.com>
2020-08-07 10:55:07 +0800

    'readme'

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,35 +1,33 @@
 <template>
   <div class="hello">
+    <div
+      id="luckysheet"
+      style="margin:0px;padding:0px;position:absolute;width:100%;height:100%;left: 0px;top: 0px;"
+    ></div>
   </div>
 </template>
 
 <script>
-// import './dist/bundle.min.js'
-
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
-    msg: String
+    msg: String,
   },
-  mounted(){
-  }
-}
+  mounted() {
+    // In some cases, you need to use $nextTick
+    // this.$nextTick(() => {
+          $(function () {
+            luckysheet.create({
+              container: "luckysheet",
+            });
+          });
+      
+    // });
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
 </style>
